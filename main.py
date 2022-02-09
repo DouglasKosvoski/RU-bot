94b89b1e9fe354549f2ca3b2f9eb21326c91d0f9
Douglas Kosvoski <douglas.contactpro@gmail.com>
2022-02-09 00:12:14 -0300

    de node para python

--- main.py
+++ main.py
@@ -1,9 +1,66 @@
 from bs4 import BeautifulSoup
-import requests
-import json
+import requests, os, json
+import discord
+from dotenv import load_dotenv
 
-def main():
-    url = 'https://www.uffs.edu.br/campi/chapeco/restaurante_universitario'
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+INVITE_LINK = os.getenv('INVITE_LINK')
+URL = os.getenv('URL')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    activeservers = client.guilds
+    for guild in activeservers:
+        print(guild.name)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content in ['!link', '!invite']:
+        await message.channel.send(INVITE_LINK)
+
+    elif message.content in ['!salve', '!ola', '!oi']:
+        await message.channel.send("Salve")
+
+    elif message.content in ["!autor", "!author"]:
+        await message.channel.send("`Douglas Kosvoski`")
+
+    elif message.content in ["!codigo", "!repo", "!repositorio"]:
+        await message.channel.send("`https://github.com/DouglasKosvoski/RU-bot`")
+
+    elif message.content in ['!menu', '!cardapio', '!cardápio']:
+        data = get_content(URL)
+        for key, value in data.items():
+            temp = ""
+            for i in value:
+                temp += i + "\n"
+            temp = key + "\n```\n" + temp + "```"
+            await message.channel.send(":fork_knife_plate: " + temp)
+
+    elif message.content in ['!cmd', '!cmds', "!commands", "!comandos"]:
+        cmds = ['!link', '!invite', '!salve', '!ola', '!oi', '!menu',
+                '!cardapio', '!cardápio', "!autor", "!author", "!codigo",
+                "!repo", "!repositorio"]
+        temp = ""
+        for i in cmds:
+            temp += i + "\n"
+        temp = f"```{temp}```"
+        await message.channel.send(temp)
+
+def get_content(url):
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
     vrau = []
@@ -12,7 +69,6 @@ def main():
         for tr in table.find_all('tr'):
             for td in tr.find_all('td'):
                 vrau.append(td.find('p').text)
-
 
     xesque = {}
     for day in range(5):
@@ -26,12 +82,11 @@ def main():
 
         xesque[list(xesque.keys())[day]] = asd
 
-    with open('output.json', 'w+') as f:
-        json.dump(xesque, f, indent=2)
+    return xesque
+
+def main():
+    client.run(TOKEN)
+
 
 if __name__ == '__main__':
     main()
-
-# token = "OTQwNDUyOTkyOTcxMzEzMTky.YgHnGg.YZGwp2Tuq9Ei654o5NKIfcdrbUY"
-# app_id = "940452992971313192"
-# invite_link = " https://discord.com/oauth2/authorize?&client_id=940452992971313192&scope=bot&permissions=8"
